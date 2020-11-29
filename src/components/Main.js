86937b13ca8f88b3c8b390addee8d459f72b8ae1
Jooyon Won <jyon1204@gmail.com>
2020-11-29 02:18:44 -0800

    Main#Style

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,10 +13,22 @@ const Main = (props) => {
         }
     }, [props])
 
+    function leadSignUpPage(e){
+        props.history.push('/signup');
+    }
+
+    function leadSignInPage(e){
+        props.history.push('/signin');
+    }
+
     return (
         <div>
             <div className="main-title">
-                {/* <h1>NuTrack</h1> */}
+                <h1>NuTrack</h1>
+                <div className="btn-wrapper">
+                    <button className="red submitBtn main-btn" onClick={(e) => leadSignUpPage(e)}>Sign Up</button>
+                    <button className="red submitBtn main-btn" onClick={(e) => leadSignInPage(e)}>Sign In</button>
+                </div>
             </div>
         </div>
     )

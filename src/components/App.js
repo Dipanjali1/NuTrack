96a7660c28c2077d6823e4f3bf84f163fbbeaae6
Jooyon Won <jyon1204@gmail.com>
2021-01-13 19:38:57 -0800

    Redirect Http to Https

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,10 @@ const App = () => {
   const [ loading, setLoading ] = useState(false);
 
   useEffect(() => {
+      let url = window.location.origin;
+      if(!url.includes('localhost') && !url.includes('https')){
+        window.location = `https:${url.split(':')[1]}`
+      }
       getUserInfo();
   }, [])
 
